79bc7762abab4e59d66db1d71153793b643fac71
Bruno Rodrigues <bruno@brodrigues.co>
2025-06-02 13:21:39 +0200

    Update generate_env_rstudio.R

--- scripts/nix_expressions/rix_intro/generate_env_rstudio.R
+++ scripts/nix_expressions/rix_intro/generate_env_rstudio.R
@@ -1,14 +1,10 @@
 library(rix)
 
-rix(r_ver = "4.3.1",
+rix(date = "2025-05-25",
     r_pkgs = c("dplyr", "ggplot2"),
     system_pkgs = NULL,
     git_pkgs = NULL,
     tex_pkgs = NULL,
-    # Rstudio is not available for macOS through Nix
     ide = "rstudio",
-    # This shell hook is necessary to start Rstudio
-    # on Linux
-    shell_hook = "export QT_XCB_GL_INTEGRATION=none",
     project_path = ".",
     overwrite = TRUE)
